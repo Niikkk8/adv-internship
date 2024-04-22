694f5b400e45abca1fed15220cd45d2811dce8d6
Niket Shah <shahniket643@gmail.com>
2024-04-22 16:31:23 +0530

    Completed achievements section, initialised footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
-
+import Footer from "@/components/Footer";
 import HomepageNavbar from "@/components/HomepageNavbar";
 import { useEffect, useState } from "react";
 import { AiFillBulb, AiFillStar } from "react-icons/ai";
+import { BiCrown, BiLeaf } from "react-icons/bi";
 import { BsMicFill } from "react-icons/bs";
+import { FaStarHalfAlt } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
 import { IoDocumentText } from "react-icons/io5";
 
 interface feature {
@@ -124,8 +127,8 @@ export default function Home() {
             {/*TESTIMONIALS SECTION*/}
             <div className="flex flex-col items-center my-12">
                 <h2 className="text-xl md:text-3xl font-bold text-center mb-8 text-[#032b41]">What our members say</h2>
-                <div className=" mb-12">
-                    <div className="w-[90%] max-w-[800px] mx-auto my-6 p-6 bg-[#fff3d7]">
+                <div className="mb-4">
+                    <div className="w-[90%] max-w-[700px] mx-auto my-6 p-6 bg-[#fff3d7]">
                         <div className="flex items-center space-x-2">
                             <h4 className="font-light text-lg text-[#032b41]">Hanna M.</h4>
                             <div className="flex text-[#0365f2]">
@@ -136,9 +139,9 @@ export default function Home() {
                                 <AiFillStar />
                             </div>
                         </div>
-                        <p className="text-sm text-[#394547]">This app has been a <strong className="font-bold">game-changer</strong> for me! It's saved me so much time and effort in reading and comprehending books. Highly recommend it to all book lovers.</p>
+                        <p className="text-md font-light text-[#394547]">This app has been a <strong className="font-bold">game-changer</strong> for me! It's saved me so much time and effort in reading and comprehending books. Highly recommend it to all book lovers.</p>
                     </div>
-                    <div className="w-[90%] max-w-[800px] mx-auto my-6 p-6 bg-[#fff3d7]">
+                    <div className="w-[90%] max-w-[700px] mx-auto my-6 p-6 bg-[#fff3d7]">
                         <div className="flex items-center space-x-2">
                             <h4 className="font-light text-lg text-[#032b41]">David B.</h4>
                             <div className="flex text-[#0365f2]">
@@ -149,9 +152,9 @@ export default function Home() {
                                 <AiFillStar />
                             </div>
                         </div>
-                        <p className="text-sm text-[#394547]">I love this app! It provides <strong className="font-bold">concise and accurate summaries</strong> of books in a way that is easy to understand. It's also very user-friendly and intuitive.</p>
+                        <p className="text-md font-light text-[#394547]">I love this app! It provides <strong className="font-bold">concise and accurate summaries</strong> of books in a way that is easy to understand. It's also very user-friendly and intuitive.</p>
                     </div>
-                    <div className="w-[90%] max-w-[800px] mx-auto my-6 p-6 bg-[#fff3d7]">
+                    <div className="w-[90%] max-w-[700px] mx-auto my-6 p-6 bg-[#fff3d7]">
                         <div className="flex items-center space-x-2">
                             <h4 className="font-light text-lg text-[#032b41]">Nathan S.</h4>
                             <div className="flex text-[#0365f2]">
@@ -162,9 +165,9 @@ export default function Home() {
                                 <AiFillStar />
                             </div>
                         </div>
-                        <p className="text-sm text-[#394547]">This app is a great way to get the main takeaways from a book without having to read the entire thing. <strong className="font-bold">The summaries are well-written and informative.</strong> Definitely worth downloading.</p>
+                        <p className="text-md font-light text-[#394547]">This app is a great way to get the main takeaways from a book without having to read the entire thing. <strong className="font-bold">The summaries are well-written and informative.</strong> Definitely worth downloading.</p>
                     </div>
-                    <div className="w-[90%] max-w-[800px] mx-auto my-6 p-6 bg-[#fff3d7]">
+                    <div className="w-[90%] max-w-[700px] mx-auto my-6 p-6 bg-[#fff3d7]">
                         <div className="flex items-center space-x-2">
                             <h4 className="font-light text-lg text-[#032b41]">Ryan R.</h4>
                             <div className="flex text-[#0365f2]">
@@ -175,11 +178,45 @@ export default function Home() {
                                 <AiFillStar />
                             </div>
                         </div>
-                        <p className="text-sm text-[#394547]">If you're a busy person who <strong className="font-bold">loves reading but doesn't have the time</strong> to read every book in full, this app is for you! The summaries are thorough and provide a great overview of the book's content.</p>
+                        <p className="text-md font-light text-[#394547]">If you're a busy person who <strong className="font-bold">loves reading but doesn't have the time</strong> to read every book in full, this app is for you! The summaries are thorough and provide a great overview of the book's content.</p>
                     </div>
                 </div>
                 <button className="bg-summarist-green w-[30%] py-2 rounded-md">Login</button>
             </div>
+            {/*ACHIEVEMENTS SECTION*/}
+            <div className="my-10">
+                <h1 className="text-xl md:text-3xl font-bold text-center mb-8 text-[#032b41]">Start growing with Summarist now</h1>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    <div className="bg-[#d7e9ff] flex flex-col justify-between px-4 py-8 rounded-xl">
+                        <BiCrown className="w-12 h-12 mx-auto text-[#0365f2]" />
+                        <div className="text-center">
+                            <h2 className="text-2xl font-semibold text-[#032b41]">3 Million</h2>
+                            <p className="text-[#394547] font-light text-md">Downloads on all platforms</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#d7e9ff] flex flex-col justify-between px-4 py-8 rounded-xl">
+                        <div className="flex justify-center items-center">
+                            <FaStar size={22} className="text-[#0365f2]" />
+                            <FaStar size={22} className="text-[#0365f2]" />
+                            <FaStar size={22} className="text-[#0365f2]" />
+                            <FaStar size={22} className="text-[#0365f2]" />
+                            <FaStarHalfAlt size={21} className="text-[#0365f2]" />
+                        </div>
+                        <div className="text-center">
+                            <h2 className="text-2xl font-semibold text-[#032b41]">4.5 Stars</h2>
+                            <p className="text-[#394547] font-light text-md">Average ratings on iOS and Google Play</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#d7e9ff] flex flex-col justify-between px-4 py-8 rounded-xl">
+                        <BiLeaf className="w-12 h-12 mx-auto text-[#0365f2]" />
+                        <div className="text-center">
+                            <h2 className="text-2xl font-semibold text-[#032b41]">97%</h2>
+                            <p className="text-[#394547] font-light text-md">Of Summarist members create a better reading habit</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
