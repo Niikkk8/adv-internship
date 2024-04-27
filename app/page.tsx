fce7bb661109594158d69d20723bc8e9ca9527cd
Niket Shah <shahniket643@gmail.com>
2024-04-27 10:18:51 +0530

    Completed login modal design

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
                     <div className="w-[80%] md:w-[50%] my-4 md:my-8 text-center md:text-left">
                         <h1 className="font-bold text-2xl md:text-4xl mb-6 w-full md:w-[75%] text-[#032b41]">Gain more knowledge in less time</h1>
                         <p className="font-light text-lg md:text-xl mb-8 text-[#394547]">Great summaries for busy people,<br /> individuals who barely have time to read,<br /> and even people who don't like to read.</p>
-                        <button className="bg-summarist-green w-[60%] py-2 rounded-md" onClick={() => dispatch(openLoginModal())}>Login</button>
+                        <button className="bg-summarist-green w-[60%] py-2 rounded-md button-transform" onClick={() => dispatch(openLoginModal())}>Login</button>
                     </div>
                     <img src="/assets/landing.png" alt="" className="w-[40%] hidden md:inline" />
                 </div>
@@ -185,7 +185,8 @@ export default function Home() {
                             <p className="text-md font-light text-[#394547]">If you're a busy person who <strong className="font-bold">loves reading but doesn't have the time</strong> to read every book in full, this app is for you! The summaries are thorough and provide a great overview of the book's content.</p>
                         </div>
                     </div>
-                    <button className="bg-summarist-green w-[30%] py-2 rounded-md">Login</button>
+                    <button className="bg-summarist-green w-[30%] py-2 rounded-md button-transform" onClick={() => dispatch(openLoginModal())}>Login</button>
+
                 </div>
                 {/*ACHIEVEMENTS SECTION*/}
                 <div className="my-10">
--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { CgClose } from 'react-icons/cg';
 import { useDispatch, useSelector } from 'react-redux';
 import { auth, provider } from '../firebase';
+import { TbUserFilled } from 'react-icons/tb';
 
 export default function LoginModal() {
     const isOpen = useSelector((state: any) => state.modals.loginModal);
@@ -57,31 +58,42 @@ export default function LoginModal() {
         <Modal open={isOpen} onClose={() => dispatch(closeLoginModal())}>
             <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center'>
                 <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50" onClick={() => dispatch(closeLoginModal())} />
-                <div className='relative w-[50%] p-8 bg-white flex flex-col items-center justify-center'>
-                    <CgClose onClick={() => dispatch(closeLoginModal())} className='absolute top-6 right-6 cursor-pointer' />
-
+                <div className='relative w-[50%] min-w-[280px] max-w-[400px] px-6 py-12 bg-white flex flex-col items-center justify-center rounded-lg'>
+                    <CgClose onClick={() => dispatch(closeLoginModal())} size={20} className='absolute top-6 right-6 cursor-pointer' />
                     {loginInterface ? (
-                        <form onSubmit={handleLoginSubmit} className="form">
-                            <h1>Log in to Summarist</h1>
-                            <button>Login as a Guest</button>
-                            <div><span /><p>or</p><span /></div>
-                            <button type="button" onClick={handleSignInWithGoogle}>Sign In with Google</button>
-                            <div><span /><p>or</p><span /></div>
-                            <input type="text" name="email" value={loginFormData.email} onChange={handleLoginInputChange} placeholder="Email" />
-                            <input type="password" name="password" value={loginFormData.password} onChange={handleLoginInputChange} placeholder="Password" />
-                            <button type="submit">Log In</button>
+                        <form onSubmit={handleLoginSubmit} className="flex flex-col w-full">
+                            <h1 className='text-center text-xl font-bold text-[#032b41] mb-4'>Log in to Summarist</h1>
+                            <button className='flex items-center justify-between my-2 button-transform bg-[#3a579d] text-white p-2 rounded-md w-full'>
+                                <TbUserFilled size={22} />
+                                <p className='mr-6'>Login as a Guest</p>
+                                <div />
+                            </button>
+                            <div className='flex items-center gap-6'><span className='h-[1px] bg-gray-400 w-[80%]' /><p className='text-gray-500'>or</p><span className='h-[1px] bg-gray-400 w-[80%]' /></div>
+                            <button className='flex items-center justify-between my-2 button-transform bg-[#4285f4] text-white p-2 rounded-md w-full' onClick={handleSignInWithGoogle}>
+                                <img src="/assets/google.png" className='w-[28px] p-1 rounded-full bg-white' alt="" />
+                                <p className='mr-6'>Log In with Google</p>
+                                <div />
+                            </button>
+                            <div className='flex items-center gap-6'><span className='h-[1px] bg-gray-400 w-[80%]' /><p className='text-gray-500'>or</p><span className='h-[1px] bg-gray-400 w-[80%]' /></div>
+                            <input type="text" name="email" value={loginFormData.email} onChange={handleLoginInputChange} placeholder="Email" className='text-sm p-2 my-1 border-2 focus:border-summarist-green outline-none rounded-md' />
+                            <input type="password" name="password" value={loginFormData.password} onChange={handleLoginInputChange} placeholder="Password" className='text-sm p-2 my-1 border-2 focus:border-summarist-green outline-none rounded-md' />
+                            <button type="submit" className='button-transform bg-summarist-green my-4 p-2 rounded-md'>Log In</button>
                         </form>
                     ) : (
-                        <form onSubmit={handleSignupSubmit} className="form">
-                            <h1>Sign up to Summarist</h1>
-                            <button type="button" onClick={handleSignInWithGoogle}>Sign In with Google</button>
-                            <div><span /><p>or</p><span /></div>
-                            <input type="text" name="email" value={signupFormData.email} onChange={handleSignupInputChange} placeholder="Email" />
-                            <input type="password" name="password" value={signupFormData.password} onChange={handleSignupInputChange} placeholder="Password" />
-                            <button type="submit">Sign Up</button>
+                        <form onSubmit={handleSignupSubmit} className="flex flex-col w-full">
+                            <h1 className='text-center text-xl font-bold text-[#032b41] mb-4'>Sign up to Summarist</h1>
+                            <button className='flex items-center justify-between my-2 button-transform bg-[#4285f4] text-white p-2 rounded-md w-full' onClick={handleSignInWithGoogle}>
+                                <img src="/assets/google.png" className='w-[28px] p-1 rounded-full bg-white' alt="" />
+                                <p className='mr-6'>Sign In with Google</p>
+                                <div />
+                            </button>
+                            <div className='flex items-center gap-6'><span className='h-[1px] bg-gray-400 w-[80%]' /><p className='text-gray-500'>or</p><span className='h-[1px] bg-gray-400 w-[80%]' /></div>
+                            <input type="text" name="email" value={signupFormData.email} onChange={handleSignupInputChange} placeholder="Email" className='text-sm p-2 my-1 border-2 focus:border-summarist-green outline-none rounded-md' />
+                            <input type="password" name="password" value={signupFormData.password} onChange={handleSignupInputChange} placeholder="Password" className='text-sm p-2 my-1 border-2 focus:border-summarist-green outline-none rounded-md' />
+                            <button type="submit" className='button-transform bg-summarist-green my-4 p-2 rounded-md'>Sign Up</button>
                         </form>
                     )}
-                    <button onClick={toggleLogin}>
+                    <button onClick={toggleLogin} className='absolute bottom-0 p-3 pt-4 text-blue-500 hover:bg-gray-300 w-full active:bg-blue-500 active:text-white'>
                         {loginInterface ? "Don't have an account?" : "Already have an account?"}
                     </button>
                 </div>
